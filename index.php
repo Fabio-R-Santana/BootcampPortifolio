ed382d697eb7ea54939babc194596ad7ac8e6116
Fabio Rafael Santana <124594142+Fabio-R-Santana@users.noreply.github.com>
2024-05-11 11:01:25 -0300

    Commit inicial

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="pt-br" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width">
+    <title>Portifolio Fábio</title>
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+
+  </head>
+  <body>
+        <header>
+            <div class="caixa">
+                <img src="Foto.jpeg" class="foto">
+                <nav>
+                    <a>Fábio Rafael Santana</a>
+                </nav>
+            </div> 
+        </header>
+        
+        <section class="texto-principal">
+            <h2 class="titulo-principal"> Sobre Mim</h2>
+                <p class="texto">Prazer!! meu nome é: <strong>Fábio Rafael Santana</strong> Tenho 17 anos, estou cursando o 3° ano do ensino médio Técnico em desenvolvimento de sistemas, Atualmente, tenho interresse de fazer uma faculdade na área de tecnologia.</p><br>
+                <p class="texto">Gosto ler, assistir filmes e séries, cozinhar, jogar, aprender coisas novas.</p><br>
+                <p class="texto">Além do desenvolvimento web, me interresei bastante pelo bootcamp Estou animado com a possibilidade de contribuir com o meu grupo, onde posso colaborar com outras pessoas talentosos e continuar aprendendo e crescendo profissionalmente, espero poder contribuir com o projeto.</p><br>
+                <p class="texto">Acredito firmemente na importância do aprendizado contínuo e estou sempre buscando aprimorar minhas habilidades e conhecimentos, seja através de cursos, projetos pessoais ou colaboração com outros profissionais.</p><br>
+                <h3 class="titulo-principal">Capacitações</h3>
+
+            <div class="conteudo-lista">
+                <ul>
+                    <li class="itens">HTML e CSS</li>
+                    <li class="itens">PYTHON</li>
+                    <li class="itens">Javascript</li>
+                    <li class="itens">C</li>
+                    <li class="itens">Programação para Robótica(Arduíno)</li>
+                </ul>
+            </div>
+
+        </section>
+
+        <footer>
+            <p>&copy;Fábio Rafael Santana</p>
+            <a class="gmail">fabiorafaelgg@gmail.com</a><br>
+            <a class="telefone">(45)99107-3944</a><br>
+            <a class="github" href="https://github.com/Fabio-R-Santana">GitHub</a><br>
+            <a class="instagram" href="https://www.instagram.com/_fabio_rs/">instagram</a>
+        </footer>
+    </body>
+</html>
